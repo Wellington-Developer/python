ef2ad65e6f2c8b5fdc313becea9f1bbc65fd4641
Wellington Santos <wellingtonsantos.developer@gmail.com>
2024-03-27 11:13:57 -0300

    feat: learning about use type function

---
+++ aula4.py
@@ -0,0 +1,16 @@
+# Tipos int e float
+# int -> Número inteiro
+# O tipo int representa qualquer número
+# positivo ou negativo, int sem sinal é considerado positivo
+print("Inteiro:", 7)
+
+# Float ->
+# float -> Número com ponto flutuante
+# O tipo float representa qualquer número
+# positivo ou negativo com ponto flutuante
+# float sem sinal é considerado positivo
+print("Float:", 7.7)
+
+# A função type mostra o tipo que o python inferiu ao valor
+print("Tipo int:", 7, type(7))
+print("Tipo float:", 7.7, type(7.7))
