d0823db648a379de149da38e2093b10b729c32c3
Wellington Santos <wellingtonsantos.developer@gmail.com>
2024-03-27 09:57:50 -0300

    feat: learning about escapes

---
+++ aula3.py
@@ -0,0 +1,20 @@
+"""
+    Python = Linguagem de programação
+    Tipo de tipagem = Dinâmica / Forte
+    str -> string -> texto
+    Strings são textos que estão dentro de aspas
+"""
+
+print(123)
+
+# Aspas simples
+print('Wellington Santos')
+
+# Aspas duplas
+print("Wellington Santos")
+
+# Escape
+print("Wellington \"Santos\"")
+
+# r
+print(r"Wellington \"Santos\"")
