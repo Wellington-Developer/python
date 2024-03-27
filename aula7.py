327403e50b79b2ca384f39ef9b9ef3d505b76fe3
Wellington Santos <wellingtonsantos.developer@gmail.com>
2024-03-27 13:19:10 -0300

    feat: learning about variables

---
+++ aula7.py
@@ -0,0 +1,7 @@
+# Variáveis são usadas para salvr algo na memória do computador
+# O sinal = é o operador de atribuição. Ele é usado para atribuir
+# um valor a um nome, no caso da declaração de variável
+
+nome_completo = "Wellington Santos"
+soma_dois_mais_dois = 2 + 2
+print(nome_completo, soma_dois_mais_dois)
