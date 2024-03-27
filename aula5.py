5bc9e6c9f9f4a8c9db790848df2bb6528d012476
Wellington Santos <wellingtonsantos.developer@gmail.com>
2024-03-27 13:03:34 -0300

    feat: learning about boolean type

---
+++ aula5.py
@@ -0,0 +1,13 @@
+# Tipo de dado bool (boolean)
+# True ou False;
+
+hacker = "Wellington"
+
+def is_hacked():
+    if(hacker == "Wellington"):
+        print("Você foi Hackeado!")
+        print(hacker == "Wellington")
+    else:
+        print("Você não foi hackeado.")
+
+is_hacked()
