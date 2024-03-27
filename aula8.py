a594bfa2505d5c9868f48e593ead718b5508b04d
Wellington Santos <wellingtonsantos.developer@gmail.com>
2024-03-27 13:25:52 -0300

    feat: exercise about variables

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = "Wellington"
+sobrenome = "Santos"
+idade = 22
+ano_nascimento = "28/03/2002"
+maior_de_idade = idade > 18
+altura_em_metros = 1.73
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Ano de nascimento:", ano_nascimento)
+print("É maior de idade?", maior_de_idade)
+print("Altura em metros:", altura_em_metros)
