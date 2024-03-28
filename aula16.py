f5bc4e18f0a8410426ad046dd64a2cb7c24bd39e
Wellington Santos <wellingtonsantos.developer@gmail.com>
2024-03-28 18:52:29 -0300

    feat: learning about if, elif and else

---
+++ aula16.py
@@ -0,0 +1,7 @@
+entrada = input("Digite entrar ou hackear: ")
+if entrada == "entrar":
+    print("Bem vindo usuário comum")
+elif entrada == "hackear":
+    print("Bem vindo Wellington!")
+else:
+    print("Decide ai amigo kk")
