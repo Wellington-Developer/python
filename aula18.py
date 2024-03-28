1ee4b5a9d84a8d33ad9c772ae31b48dd2aa42dd8
Wellington Santos <wellingtonsantos.developer@gmail.com>
2024-03-28 19:12:10 -0300

    feat: learning about debugger python

---
+++ aula18.py
@@ -0,0 +1,15 @@
+condicao_um = False;
+condicao_dois = False;
+condicao_tres = True;
+condicao_quatro = False;
+
+if condicao_um:
+    print("Condição 1 foi chamada")
+elif condicao_dois:
+    print("Condição 2 foi chamada")
+elif condicao_tres:
+    print("Condição 3 foi chamada")
+elif condicao_quatro:
+    print("Condição 4 foi chamada")
+else:
+    print("Nenhuma condição foi chamada")
