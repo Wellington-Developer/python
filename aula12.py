3d2c278a05e3754d1702951251490ed4cdecfc36
Wellington Santos <wellingtonsantos.developer@gmail.com>
2024-03-28 18:00:36 -0300

    feat: exercise about IMC

---
+++ aula12.py
@@ -0,0 +1,8 @@
+nome = "Wellington Santos"
+altura = 1.75
+peso = 75
+
+# Wellington Santos tem 1.75 de altura, pesa 75kg e seu IMC é...
+
+imc = peso / (altura * altura)
+print(nome, "tem", altura, "de altura, pesa", peso, "e seu IMC é", imc)
